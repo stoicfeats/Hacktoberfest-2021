5640ca8eea2c2c5f74c1139a34dd0f7a2a2eb043
TheHanyu <91917207+TheHanyu@users.noreply.github.com>
2021-10-20 11:36:21 -0700

    Create hobby finder.py

---
+++ Python/hobby finder.py
@@ -0,0 +1,12 @@
+print("Let's find you a hobby!")
+print("do you like to go outside? yes or no")
+outside=input()
+if outside = "yes"
+  print("try out riding a bike!")
+  else
+    print("do you like videogames? yes or no")
+    videogames=input()
+    if videogames = "yes"
+      print("try out Call of Duty or Fifa22!")
+      else
+        print("try hanging out with friends!")
