02107683ed349a23c2fc50cef911cc9c551c4978
Shamik Das <83940432+Shamik1836@users.noreply.github.com>
2021-10-25 00:02:05 +0530

    Added a file demonstrating how to take user inputs using Scanner

---
+++ Java/Taking_User_input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String str=sc.nextLine();
+        System.out.println("Your name is "+str);
+    }
+}
